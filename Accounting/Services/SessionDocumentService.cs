9c90f30f5b2dfbf407fbd1def56225c6644c6ed0
svyatick <casemiron44@gmail.com>
2022-10-22 15:50:11 +0300

    Рефактор сервиса для работы с сессией

--- Accounting/Services/SessionDocumentService.cs
+++ Accounting/Services/SessionDocumentService.cs
@@ -15,18 +15,9 @@ namespace Accounting.Services
 
         public async Task<bool> AddEmployee(NotBetEmployee employee)
         {
-            try
-            {
-                var sessionDocument = _session.GetJson<SessionDocument>(sessionDocumentKey) ?? new SessionDocument();
-                sessionDocument.Employees.Add(this.MapToSessionEmployee(employee));
-                _session.SetJson(sessionDocumentKey, sessionDocument);
-                await _session.CommitAsync();
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
+            var sessionDocument = GetSessionDocument();
+            sessionDocument.Employees.Add(this.MapToSessionEmployee(employee));
+            return await Commit(sessionDocument);
         }
         public async Task<bool> Clear()
         {
@@ -43,99 +34,65 @@ namespace Accounting.Services
         }
         public Document GetDocument(DocumentViewModel documentViewModel)
         {
-            var sessionDocument = _session.GetJson<SessionDocument>(sessionDocumentKey);
+            var sessionDocument = GetSessionDocument();
             var document = new Document(this.MapToListFromSessionEmployee(sessionDocument.Employees),this.MapToListFromSessionAccrual(sessionDocument.Accruals)
                 ,documentViewModel.Name, documentViewModel.DateCreate.Date);
             return document;
         }
 
-        public async Task<bool> CreateSessionDocument()
-        {
-            try
-            {
-                _session.SetJson(sessionDocumentKey, new SessionDocument());
-                await _session.CommitAsync();
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
+        public async Task<bool> CreateSessionDocument() => await Commit(new SessionDocument());
+
         public List<Accrual> GetAccrualsByEmployeeId(Guid employeeId)
         {
-            var sessionAccruals = _session.GetJson<SessionDocument>(sessionDocumentKey).Accruals.Where(x => x.EmployeeId == employeeId);
+            var sessionAccruals = GetSessionDocument().Accruals.Where(x => x.EmployeeId == employeeId);
             if (sessionAccruals is not null)
                 return this.MapToListFromSessionAccrual(sessionAccruals.ToList());
             return null;
         }
+
         public async Task<bool> AddAccrual(Accrual accrual)
         {
-            try
-            {
-                var sessionDocument = _session.GetJson<SessionDocument>(sessionDocumentKey);
-                sessionDocument.Accruals.Add(this.MapToSessionAccrual(accrual));
-                _session.SetJson(sessionDocumentKey, sessionDocument);
-                await _session.CommitAsync();
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
+            var sessionDocument = GetSessionDocument();
+            sessionDocument.Accruals.Add(this.MapToSessionAccrual(accrual));
+            return await Commit(sessionDocument);
         }
+
         public decimal SumOfAccruals() => _session.GetJson<SessionDocument>(sessionDocumentKey).Accruals.Sum(x => x.Ammount);
+
         public async Task<bool> DeleteEmployee(Guid id)
         {
-            try
-            {
-                var sessionDocument = _session.GetJson<SessionDocument>(sessionDocumentKey);
-                sessionDocument.Employees.Remove(sessionDocument.Employees.FirstOrDefault(x => x.Id == id));
-                _session.SetJson(sessionDocumentKey, sessionDocument);
-                await _session.CommitAsync();
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
+            var sessionDocument = GetSessionDocument();
+            sessionDocument.Employees.Remove(sessionDocument.Employees.FirstOrDefault(x => x.Id == id));
+            return await Commit(sessionDocument);
         }
+
         public async Task<bool> UpdateAccrual(decimal ammount, Guid accrualId)
         {
-            try
-            {
-                var sessionDocument = _session.GetJson<SessionDocument>(sessionDocumentKey);
-                sessionDocument.Accruals.SingleOrDefault(x => x.Id == accrualId).Ammount = ammount;
-                _session.SetJson(sessionDocumentKey, sessionDocument);
-                await _session.CommitAsync();
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
+            var sessionDocument = GetSessionDocument();
+            sessionDocument.Accruals.SingleOrDefault(x => x.Id == accrualId).Ammount = ammount;
+            return await Commit(sessionDocument);
         }
+
         public async Task<bool> DeleteAccrualsByEmployeeId(Guid id)
         {
-            try
-            {
-                var sessionDocument = _session.GetJson<SessionDocument>(sessionDocumentKey);
-                sessionDocument.Accruals.RemoveAll(x => x.EmployeeId == id);
-                _session.SetJson(sessionDocumentKey, sessionDocument);
-                await _session.CommitAsync();
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
+            var sessionDocument = GetSessionDocument();
+            sessionDocument.Accruals.RemoveAll(x => x.EmployeeId == id);
+            return await Commit(sessionDocument);
         }
+
         public async Task<bool> DeleteAccrual(Guid accrualId)
+        {
+            var sessionDocument = GetSessionDocument();
+            sessionDocument.Accruals.RemoveAll(x => x.Id == accrualId);
+            return await Commit(sessionDocument);
+        }
+
+        private SessionDocument GetSessionDocument() => _session.GetJson<SessionDocument>(sessionDocumentKey);
+
+        private async Task<bool> Commit(SessionDocument sessionDocument)
         {
             try
             {
-                var sessionDocument = _session.GetJson<SessionDocument>(sessionDocumentKey);
-                sessionDocument.Accruals.RemoveAll(x => x.Id == accrualId);
                 _session.SetJson(sessionDocumentKey, sessionDocument);
                 await _session.CommitAsync();
                 return true;
@@ -159,6 +116,7 @@ namespace Accounting.Services
         }
         
         private Accrual MapFromSessionAccrual(SessionAccrual sessionAccrual) => new Accrual(sessionAccrual.DateCreate, sessionAccrual.Ammount, sessionAccrual.Id);
+
         private SessionNotBetEmployee MapToSessionEmployee(NotBetEmployee employee)
         {
             var sessionEmployee = new SessionNotBetEmployee()
@@ -169,12 +127,14 @@ namespace Accounting.Services
             };
             return sessionEmployee;
         }
+
         private NotBetEmployee MapFromSessionEmployee(SessionNotBetEmployee sessionEmployee)
         {
             var employee = new NotBetEmployee(sessionEmployee.Name, sessionEmployee.InnerId);
             employee.SetId(sessionEmployee.Id);
             return employee;
         }
+
         private List<NotBetEmployee> MapToListFromSessionEmployee(List<SessionNotBetEmployee> sessionEmployees)
         {
             List<NotBetEmployee> employees = new List<NotBetEmployee>();
@@ -182,6 +142,7 @@ namespace Accounting.Services
                 employees.Add(this.MapFromSessionEmployee(item));
             return employees;
         }
+
         private List<Accrual> MapToListFromSessionAccrual(List<SessionAccrual> sessionAccruals)
         {
             List<Accrual> accruals = new List<Accrual>();
