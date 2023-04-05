0a09717f5cc5de30675d8b38fca7952ab4a16615
svyatick <casemiron44@gmail.com>
2023-04-05 19:29:06 +0300

    фикс багов

---
+++ Accounting/ModelBinders/DecimalModelBinder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Globalization;
+
+namespace Accounting.ModelBinders
+{
+    public class DecimalModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+
+            if (valueProviderResult != ValueProviderResult.None && decimal.TryParse(valueProviderResult.FirstValue, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out var result))
+            {
+                bindingContext.Result = ModelBindingResult.Success(result);
+            }
+            else
+            {
+                bindingContext.Result = ModelBindingResult.Failed();
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
--- Accounting/ViewModels/PayoutViewModel.cs
+++ Accounting/ViewModels/PayoutViewModel.cs
@@ -1,9 +1,19 @@
-﻿namespace Accounting.Domain.ViewModels
+﻿using Accounting.ModelBinders;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Accounting.Domain.ViewModels
 {
     public class PayoutViewModel
     {
         public Guid EmployeeId { get; set; }
+
         public bool IsAdditional { get; set; }
+
+        [BindProperty(BinderType = typeof(DecimalModelBinder))]
+        [DisplayFormat(DataFormatString = "{0:F2}", ApplyFormatInEditMode = true)]
         public decimal Ammount { get; set; }
     }
+
+    
 }
